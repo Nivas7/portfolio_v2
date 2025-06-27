ea5d7ed090e20fc986515c7d8fcb2c76a876efa9
Srinivas <nivasniyas24@gmail.com>
2025-06-27 20:57:05 +0530

    Fixing all the files

---
+++ data/experience.ts
@@ -0,0 +1,21 @@
+export type Experience = {
+    company: string,
+    info: string,
+    link: string,
+    logo: string
+}
+
+export const experience: Experience[] = [
+    {
+        company: "Solo Securities",
+        info: "Full Stack Intern | Oct 2024 - Dec 2024 | Remote | Internship",
+        link: "https://solosecurities.com/",
+        logo: "/company-logos/SoloSecuritiesLogo.svg"
+    },
+    {
+        company: "NSIC",
+        info: "Full Stack Intern | Jul 2023 - Aug 2023 | Remote | Internship",
+        link: "https://www.nsic.co.in/",
+        logo: "/company-logos/newNS_70Years.png"
+    }
+]
---
+++ lib/constants.ts
@@ -0,0 +1,8 @@
+export const SocialLinks = {
+    github: "https://github.com/nivas7",
+    x: "https://x.com/Nivas124",
+    linkedin: "https://www.linkedin.com/in/nivaz/",
+    discord: "https://discord.com/users/srinivas_7",
+    instagram: "https://www.instagram.com/nivaz.xo",
+    reddit: "https://reddit.com/u/Niva_z"
+}
